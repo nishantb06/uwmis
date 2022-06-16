cc7ecaad0fbcc2bd8c126c8e743d1fb0761f782c
nishantb06 <nbhansali06@gmail.com>
2022-06-17 00:09:15 +0530

    glob error resolved

--- src/dataloader.py
+++ src/dataloader.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import torch 
 import cv2
-import glob
+from glob import glob
 
 from torch.utils.data import Dataset, DataLoader
 import albumentations as A
@@ -40,7 +40,7 @@ def get_mask_paths():
 
 def get_mask_paths_25D():
     try:
-        path_df = pd.DataFrame(glob('../input/uwmgi-25d-stride2-dataset/images/images/*'), columns=['image_path'])
+        path_df = pd.DataFrame(glob('/kaggle/input/uwmgi-25d-stride2-dataset/images/images/*'), columns=['image_path'])
         path_df['mask_path'] = path_df.image_path.str.replace('image','mask')
         path_df['id'] = path_df.image_path.map(lambda x: x.split('/')[-1].replace('.npy',''))
         print("new Mask path file uploaded")
